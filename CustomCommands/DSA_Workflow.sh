17687a0c7d45443d75c933d57c7a09f6a38f6d0e
Uday Beswal <71140717+UdBe@users.noreply.github.com>
2023-07-17 21:41:06 +0530

    Add files via upload

---
+++ CustomCommands/DSA_Workflow.sh
@@ -0,0 +1,5 @@
+#! /usr/bin/bash
+set -e
+code /home/uday/Desktop/DSA  
+brave-browser https://www.youtube.com/playlist?list=PLDzeHZWIZsTryvtXdMr6rPh4IDexB5NIA https://www.geeksforgeeks.org/data-structures/?ref=shm https://leetcode.com/problemset/all/ https://github.com/UdBe/DSA-LoveBabbar || exit
+
